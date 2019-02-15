2b35e82fe052b299d1d0774368d69fe0b41ce88e
aichingerj <aichingerj@comcast.net>
2019-02-15 15:59:42 -0500

    adding all the project folders and outline for project 1

---
+++ Project 1/main.py
@@ -0,0 +1,11 @@
+#PCR = denaturation, annealing, extension
+#denaturing- heats it up and splits the dna in to the two seperate strands
+#annealing- primers bind to a matching sections
+#extension- starting from the primers the dna starts to be replicated by matching the base pairs of the template DNA.
+
+#our template dna is 2000 base pairs long (n). This is created randomly, made up of A,T,C,G. (basically denaturing but not actually)
+#our primers are 20 base pairs long and randomly generated.
+#these primers will match up to a sequence in the template dna of 2000 base pairs.
+#the primer then binds (annealing)
+#now the dna can be replicated starting from the primer. This will go for a segment of about 200 (m)
+#the stopping point of the replication/amplification is determined by d+r, d is equivalent to 200 (m) and r is random generated number between 50 and -50. So total length will be between 150 and 250.
