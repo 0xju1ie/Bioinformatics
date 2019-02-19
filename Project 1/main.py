2b68b1467ea02389dda76876e33319364bd6f0da
Laverena Wienclaw <laverenawienclaw@MacBook-Pro-4.local>
2019-02-19 00:52:14 -0500

    Initial workings

--- Project 1/main.py
+++ Project 1/main.py
@@ -1,11 +1,55 @@
-#PCR = denaturation, annealing, extension
-#denaturing- heats it up and splits the dna in to the two seperate strands
-#annealing- primers bind to a matching sections
-#extension- starting from the primers the dna starts to be replicated by matching the base pairs of the template DNA.
+from random import choice, randint
 
-#our template dna is 2000 base pairs long (n). This is created randomly, made up of A,T,C,G. (basically denaturing but not actually)
-#our primers are 20 base pairs long and randomly generated.
-#these primers will match up to a sequence in the template dna of 2000 base pairs.
-#the primer then binds (annealing)
-#now the dna can be replicated starting from the primer. This will go for a segment of about 200 (m)
-#the stopping point of the replication/amplification is determined by d+r, d is equivalent to 200 (m) and r is random generated number between 50 and -50. So total length will be between 150 and 250.
+# Create complement of DNA
+dna = ""
+count = 1
+for count in range(400):
+ dna += choice("ATCG")
+# print(dna)
+
+primer1 = dna[300:400]
+# print(primer1)
+
+print("\n\n")
+
+
+# index = ""
+# for index in dna:
+
+#     if index == "A":
+#         print("T")
+#     elif index == "T":
+#         print("A")
+#     elif index == "C":
+#         print("G")
+#     elif index == "G":
+#         print("C")
+
+
+
+print("testing now")
+
+primer1 = dna[300:400]
+print(primer1)
+
+
+# Create complement of DNA
+def complement_dna(self, index):
+    index = ""
+    new_dna = ""
+    for index in self:
+        if index == "A":
+            new_dna += "T"
+        if index == "T":
+            new_dna += "A"
+        if index == "C":
+            new_dna += "G"
+        if index == "G":
+            new_dna += "C"
+    return new_dna
+
+
+complemented = complement_dna(primer1, len(primer1))
+
+print(complemented)
+
