3dc79293c81dd79eccaca3e8b7270243f8083f5e
aichingerj <aichingerj@comcast.net>
2019-02-19 22:49:57 -0500

    final upload

--- Project 1/main.py
+++ Project 1/main.py
@@ -33,7 +33,7 @@ import queue
 FRAGMENT_LEN = 200
 MAX_RAND_LEN = 100
 PRIMER_LEN = 20
-NUM_CYCLES = 7
+NUM_CYCLES = 8
 
 
 # Generate random DNA strand, n, of length 2000
@@ -62,8 +62,8 @@ def generate_primer(dna):
   while is_unique != 1: #of that 200, we need to get the primer of 20
     primer = section_complement[primer_start:primer_end] #search this complement for a match of the pirmer
     is_unique = complementary_dna.count(primer)
-    primer_start =+ 1
-    primer_end =+ 1
+    primer_start += 1
+    primer_end += 1
   return primer
 
 
@@ -100,6 +100,11 @@ complimentary_dna = complement_dna(dna, 0)
 print("DNA strand 1: ", dna[300:500])
 print("DNA strand 2: ", complimentary_dna[300:500])
 
+print("generating Primers for each strand") 
+primer1 = generate_primer(dna) 
+primer2 = generate_primer(complimentary_dna) #could also do complement_dna(primer1, 0) 
+print("primer 1: ", primer1) 
+print("primer 2: ", primer2)
 
 #getting the length of the first strands to replicate
 fall_off_rate = d_plus_r()
@@ -113,10 +118,10 @@ complimentary_dna_section = complimentary_dna[300:end]
 
 print("annealing - 55 degrees celcius. Binding the primers to the DNA strands") 
 
-current_dna_q = queue.Queue(2000)
-current_primer_q = queue.Queue(2000)
-new_dna_q = queue.Queue(2000)
-new_primer_q = queue.Queue(2000)
+current_dna_q = queue.Queue(0)
+current_primer_q = queue.Queue(0)
+new_dna_q = queue.Queue(0)
+new_primer_q = queue.Queue(0)
 
 current_dna_q.put(dna_section) #comes out first
 current_dna_q.put(complimentary_dna_section) 
@@ -127,6 +132,16 @@ current_primer_q.put(primer2)
 replicant = ""
 compliment = ""
 
+counter_150_to_159 = 0
+counter_160_to_169 = 0
+counter_170_to_179 = 0
+counter_180_to_189 = 0
+counter_190_to_199 = 0
+counter_200_to_209 = 0
+counter_210_to_219 = 0
+counter_220_to_229 = 0
+counter_230_to_239 = 0
+counter_240_to_250 = 0
 
 # Replication loop:
 # need to check that the strand is long enough to duplicate
@@ -141,7 +156,9 @@ for i in range(NUM_CYCLES):
             if contains == 1:
                 print("yay, long enough!")
                 can_duplicate = True
-                compliment = template[20:]
+                compliment_end = d_plus_r()
+                print("d+r = ", compliment_end)
+                compliment = template[20:compliment_end]
                 compliment =complement_dna(compliment, 0)
                 print("compliment:", compliment)
                 replicant = primer + compliment
@@ -153,22 +170,50 @@ for i in range(NUM_CYCLES):
                 print("replicant Primer: ", replicant_primer)
                 new_primer_q.put(primer)
                 new_primer_q.put(replicant_primer)
-                
-                size = current_dna_q.qsize()
-                print("current dns: ", size)
-                size = new_dna_q.qsize()
-                print("new dna:", size)
+
             else:
                 print("too short")
                 can_duplicate = False
             can_duplicate = False
-    print("here")
-    size = new_dna_q.qsize()
-    print(size)
     while new_dna_q.qsize() != 0:
-        print("in the while loop")
         current_dna_q.put(new_dna_q.get())
         current_primer_q.put(new_primer_q.get())
+print("size: ", current_dna_q.qsize())
+while current_dna_q.qsize() != 0:  
+    popped = current_dna_q.get()
+    size_of_popped = len(popped)
+    print("size of popped: ", size_of_popped)
+    if size_of_popped in range(150, 160):
+        counter_150_to_159 += 1
+    elif size_of_popped in range(160, 170):
+        counter_160_to_169 += 1
+    elif size_of_popped in range(170, 180):
+        counter_170_to_179 += 1
+    elif size_of_popped in range(180, 189):
+        counter_180_to_189 += 1
+    elif size_of_popped in range(190, 200):
+        counter_190_to_199 += 1
+    elif size_of_popped in range(200, 210):
+        counter_200_to_209 += 1
+    elif size_of_popped in range(210, 220):
+        counter_210_to_219 += 1
+    elif size_of_popped in range(220, 230):
+        counter_220_to_229 += 1
+    elif size_of_popped in range(230, 240):
+        counter_230_to_239 += 1
+    elif size_of_popped in range(240, 250):
+        counter_240_to_250 += 1
+    print("size of it now: ", current_dna_q.qsize())
+print("counter_150_to_159: ", counter_150_to_159)
+print("counter_160_to_169: ", counter_160_to_169)
+print("counter_170_to_179: ", counter_170_to_179)
+print("counter_180_to_189: ", counter_180_to_189)
+print("counter_190_to_199: ", counter_190_to_199)
+print("counter_200_to_209: ", counter_200_to_209)
+print("counter_210_to_219: ", counter_210_to_219)
+print("counter_220_to_229: ", counter_220_to_229)
+print("counter_230_to_239: ", counter_230_to_239)
+print("counter_240_to_250: ", counter_240_to_250) 
        
  # need to empty the new qs into the current q
  # decrease cycle       
